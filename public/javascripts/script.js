b31cdef4ce3f11d99554372bfe3529378078db82
victor miclovich <vicmiclovich@gmail.com>
2015-04-03 10:06:54 -0700

    discussed code

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,17 +1,20 @@
 $(document).ready(function(){
 
-	
+
 // $('.taskname').on('click', function () {
-	
+
 // var task = $(this).data('pk');
 	$('.taskname').editInPlace({
 		callback : function (unused, enteredText) {
+			var payload = $(this).text();
+			var project_id = window.location.pathname.substring("/projects/".length);
 			$.ajax({
 			  type: "POST",
 			  // url: "/projects/551eb14bd2259516ac7d7887",
-			  url: '/tasks/' + $(this).data('pk'),
-			  data: $(this).text(),
-			  success: console.log('/tasks/' + $(this).data('pk')),
+			  // url: '/projects/' + $(this).data('pk'),
+				url: '/projects/' + project_id,
+			  data: {taskname: payload, project_id: project_id}, // JSON.stringify
+			  success: null, //console.log('/tasks/' + $(this).data('pk')),
 			  dataType: 'json'
 			});
 
